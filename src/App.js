72a9cbebb436c29231a04d88ec048f619fef7c6c
Young312886 <hongym0497@gmail.com>
2023-01-15 18:41:21 +0900

    오타 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 import Coins from "./routes/Coins";
@@ -8,20 +8,18 @@ import Todos from "./routes/Todos";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/todos">
-            <Todos />
-        </Route>
-        <Route path="/coins">
-            <Coins />
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Route path="/todos">
+        <Todos />
+      </Route>
+      <Route path="/coins">
+        <Coins />
+      </Route>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
     </Router>
   );
 }
